e7679a0a3c7e3c8908c45eb63964d27498b99065
Moura Yuri <mourayuri339@gmail.com>
2020-08-02 22:28:32 -0300

    fixed problem with retrieving template from frame

--- templateMatching.py
+++ templateMatching.py
@@ -1,8 +1,16 @@
 import cv2 as cv
+import numpy as np
+
+def searchTemplate(template,img):
+    flattenTemplate = np.array(template).flatten()
+    for i in range(len(img)-len(template)):
+        for j in range(len(img[0])-len(template[0])):
+            return
 
 cap = cv.VideoCapture('Taiwan.mp4')
 
 roi = None
+template = None
 while(cap.isOpened()): 
       
   # Capture frame-by-frame 
@@ -12,10 +20,13 @@ while(cap.isOpened()):
     frame = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
     if roi ==None:
         roi = cv.selectROI(frame)
-    
+        #template = frame[roi[0]:roi[0]+roi[2],roi[1]:roi[1]+roi[3]]
+        template = frame[roi[1]:roi[1]+roi[3],roi[0]:roi[0]+roi[2]]
+
+    newFrame = searchTemplate(template, frame)
+
     startPoint = (roi[0], roi[1])
     endPoint = (roi[0]+roi[2], roi[1]+roi[3])
-    print("roi: ", roi)
     cv.rectangle(frame,startPoint,endPoint,(0,255,0), 2)
     
     # Display the resulting frame 
